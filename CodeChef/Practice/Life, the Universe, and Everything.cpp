117015ff1016d020152284fdceff675775d328dd
YashSingh15 <83363915+YashSingh15@users.noreply.github.com>
2021-09-10 00:05:48 +0530

    Create Life, the Universe, and Everything.cpp

---
+++ CodeChef/Practice/Life, the Universe, and Everything.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+    int a;
+
+    while(cin >> a){
+        if(a != 42){
+            cout << a << endl;
+        }else{
+            break;
+        }
+    }
+
+
+    return 0;
+}
