a9d4cbb2e25e0cb2963d3db1988a21c20a5d6c80
YashSingh15 <83363915+YashSingh15@users.noreply.github.com>
2021-09-10 02:07:04 +0530

    Create Word Capitalization.cpp

---
+++ CodeForces/Practice/Word Capitalization.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void sortArray(int );
+
+int main(){
+
+    string s;
+    cin >> s;
+
+    s[0] = char(toupper(s[0]));
+
+    cout << s;
+
+    return 0;
+}
