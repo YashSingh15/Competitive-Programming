dad7ac4633ee452ac208e256e3635a365cee4b7c
YashSingh15 <83363915+YashSingh15@users.noreply.github.com>
2021-09-09 22:24:31 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,29 @@ using namespace std;
 
 int main() {
 
-    
+    int T;
+    cin >> T;
+    int N, A, B;
+    string s;
+
+    int time;
+
+    for(int i = 0; i < T; i++){
+        cin >> N >> A >> B;
+        cin >> s;
+        time = 0;
+
+        for(int j = 0; j < N; j++){
+            if(s[j] == '0'){
+                time += A;
+            }
+            else{
+                time += B;
+            }
+        }
+
+        cout << time << endl;
+    }
 
     return 0;
 }
