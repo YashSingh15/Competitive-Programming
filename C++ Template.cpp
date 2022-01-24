ae5987e4ff7558b0f941ad196a9d6c545b112477
YashSingh15 <83363915+YashSingh15@users.noreply.github.com>
2022-01-24 17:02:20 +0530

    Create C++ Template.cpp

---
+++ C++ Template.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define REP(i, a, b) for(int (i) = (a); (i) < (b); (i)++)
+#define rREP(i, a, b) for(int (i) = (a) - 1; (i) >= (b); (i)--)
+#define PB push_back
+#define all(x) (x).begin(), (x).end()
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef set<int> si;
+typedef pair<int, int> pii;
